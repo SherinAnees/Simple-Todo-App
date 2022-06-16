bcbafea921b1855766fafcd5b209833220beb286
Sherin <csherin111@gmail.com>
2022-06-17 01:23:36 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function Todo({ todo, index, markTodo, removeTodo }) {
           ✓ Done
         </Button>{" "}
         <Button variant="outline-danger" onClick={() => removeTodo(index)}>
-          ✕ Delete
+          ✕ Delet
         </Button>
       </div>
     </div>
